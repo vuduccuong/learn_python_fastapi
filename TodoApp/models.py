2754f47b1a9992c49cee422edca027848f158ba6
Vu Duc Cuong <vuduccuong.ck48@gmail.com>
2022-09-25 16:47:21 +0700

    Feat: Add models

---
+++ TodoApp/models.py
@@ -0,0 +1,32 @@
+#  author = "Vũ Đức Cường"
+#  date = 9/23/22, 9:09 PM
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from TodoApp.database import Base
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100))
+    description = Column(String(255))
+    priority = Column(Integer, default="")
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="todos")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255))
+    username = Column(String(50))
+    first_name = Column(String(100))
+    last_name = Column(String(100))
+    hash_password = Column(String(255))
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship("Todo", back_populates="owner")
